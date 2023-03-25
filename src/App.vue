7b759a3bcb0dc23781e358b5b7e2f3c70661d38a
shenghai <shenghairen@gmail.com>
2023-03-25 21:08:45 +0800

    jialetest

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Mint />
+</template>
+
+<script>
+import Mint from './components/Mint.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Mint
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+html,
+body {
+
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
---
+++ src/components/Mint.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <div class="hello">
+        <img class="backgroundimg" src="https://pbs.twimg.com/profile_banners/1308196317852655618/1614891929/1500x500" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.backgroundimg {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.hello {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+}
+</style>
